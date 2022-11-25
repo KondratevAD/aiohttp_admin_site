0995a73e347eb41031f97db7fdca56e7d7ed7d74
KondratevAD <kondratev_ad@bk.ru>
2022-11-25 18:25:38 +0300

    add validation intField

--- aiohttp_admin_site/mappers/validators/integer_value.py
+++ aiohttp_admin_site/mappers/validators/integer_value.py
@@ -11,12 +11,11 @@ def integer(*, max_value: int = None, min_value: int = None):
             field_name = IntField(validators=[integer(max_value=5)])
     """
     def integer_validator(value) -> None:
-        if max_value and value > max_value:
+        if value and max_value is not None and value > max_value:
             raise ValidationError(
                 f"'{value}' cannot be greater than {max_value}"
             )
-
-        if min_value and value < min_value:
+        if value and min_value is not None and value < min_value:
             raise ValidationError(
                 f"'{value}' cannot be less than {min_value}"
             )
